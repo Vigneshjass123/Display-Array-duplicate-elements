0270b9c1cf420c8709cd77c7ab1cc9eaab015bf5
Cyntexia <Cyntexia@DESKTOP-R5AAUJH>
2023-06-13 23:28:49 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Display_Array_duplicate_elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5,6,7,8,9, 1, 2, 3 };
+            
+            for(int i =0;i<arr.Length;i++)
+
+            {
+                for(int j =i+1;j<arr.Length;j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+
+
+                        Console.Write(arr[i]);
+                    }
+                   
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
